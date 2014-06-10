9c59c17bb58fe63badfeed549555542f9d57296b
Msayasu Kawamura <masaka@gmail.com>
2014-06-10 14:02:55 +0900

    first commt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoge::Application.config.secret_token = '20c6b7acbb272190bd329eeba80b88fc5407b3a855d9d26262afca60c0cdbaea9b263fa09ab07965af9e3b636d7d1e7d2db915d6edbded3d28fa78c068276225'
